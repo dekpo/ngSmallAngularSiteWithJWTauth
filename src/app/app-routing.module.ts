e88a8390ef0ad23fc1aeb7056430bfc67f033ede
Dekpo <dekpo@me.com>
2021-05-06 16:40:48 +0200

    Created and used admin Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { AdminComponent } from './admin/admin.component';
 import { BlogComponent } from './blog/blog.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'blog', component: BlogComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'admin', component: AdminComponent}
+  {path: 'admin', component: AdminComponent,
+  canActivate: [AuthGuard]}
 ];
 
 @NgModule({
