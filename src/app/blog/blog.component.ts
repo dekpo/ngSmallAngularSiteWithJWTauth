4f454d0f49456e32a37b5957be8f2ab1f14f5527
Dekpo <dekpo@me.com>
2021-05-06 14:58:24 +0200

    Made a little ng app ...

---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css']
+})
+export class BlogComponent implements OnInit {
+
+  posts:any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:443/posts').subscribe((data)=>{
+    console.log(data);
+    this.posts = data;
+    });
+    console.log(localStorage.getItem('TOKEN'));
+  }
+
+}
