c95d1db8f7ef6a54da6384efe8dee64d198d76a7
Dekpo <dekpo@me.com>
2021-05-06 19:04:51 +0200

    Made few changes to admin form

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-admin',
@@ -9,7 +13,30 @@ import { AuthService } from '../services/auth.service';
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  postForm: FormGroup;
+
+  constructor(private helper: JwtHelperService,private authService: AuthService, private router: Router, private http: HttpClient, private formBuilder: FormBuilder) { }
+
+  post(){
+    // console.log(this.authService.url+'/posts => ',this.postForm.value);
+    this.send(this.authService.url+'/posts').subscribe();
+    const token = localStorage.getItem('access_token');
+    const exp = this.helper.getTokenExpirationDate(token);
+    console.log('Expiration ',exp);
+  }
+
+  send(post){
+    return this.http.post(post, this.postForm.value).pipe(
+      tap( res =>{
+        console.log('Post Res:',res);
+        this.router.navigateByUrl('/blog');
+      }),
+      catchError( e =>{
+        alert(e.error.message);
+        throw e;
+      })
+    )
+  }
 
   logout(){
     this.authService.logout();
@@ -17,6 +44,10 @@ export class AdminComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.postForm = this.formBuilder.group({
+      title: ['',[Validators.required]],
+      text: ['',[Validators.required]]
+    });
   }
 
 }
